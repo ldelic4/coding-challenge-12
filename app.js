c15d0dfc6bd34eae7c8b662fd6fc10a4742b6b32
Lea Delic <leadelic0429@gmail.com>
2024-10-27 12:52:45 -0400

    : Add canvas setup and event handling logic.

---
+++ app.js
@@ -0,0 +1,9 @@
+//task 2
+// Get the canvas element
+const canvas = document.getElementById('myCanvas');
+
+// Get the 2D drawing context
+const ctx = canvas.getContext('2d');
+// mouse events to handle drawing
+canvas.addEventListener('mouseup',()=>{
+})
